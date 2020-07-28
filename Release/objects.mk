006db44a02248120e2162926a4d5ef86ec844020
Stephen Via <stephenvia3@gmail.com>
2020-07-28 16:39:32 -0400

    fixed release ignore

--- Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/ti/simplelink_cc13x2_26x2_sdk_4_10_00_78/source/ti/ti154stack/library/tirtos/ccs/bin/maclib_nosecure_cc13x2.a" -l"C:/ti/simplelink_cc13x2_26x2_sdk_4_10_00_78/source/ti/ti154stack/library/tirtos/ccs/timac_rom_PG2_0_rom_api_linker.cmd" -l"C:/ti/simplelink_cc13x2_26x2_sdk_4_10_00_78/source/ti/display/lib/display.aem4f" -l"C:/ti/simplelink_cc13x2_26x2_sdk_4_10_00_78/source/ti/grlib/lib/ccs/m4f/grlib.a" -l"C:/ti/simplelink_cc13x2_26x2_sdk_4_10_00_78/source/ti/drivers/rf/lib/rf_multiMode_cc13x2.aem4f" -l"C:/ti/simplelink_cc13x2_26x2_sdk_4_10_00_78/source/ti/drivers/lib/drivers_cc13x2.aem4f" -l"C:/ti/simplelink_cc13x2_26x2_sdk_4_10_00_78/kernel/tirtos/packages/ti/dpl/lib/dpl_cc13x2.aem4f" -l"C:/ti/simplelink_cc13x2_26x2_sdk_4_10_00_78/source/ti/devices/cc13x2_cc26x2/driverlib/bin/ccs/driverlib.lib" -llibc.a
-
